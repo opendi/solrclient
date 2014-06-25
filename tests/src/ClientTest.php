21e8fb712d5a210d9b83c955f5a2eede99308839
Ivan Habunek <ivan.habunek@gmail.com>
2014-06-25 13:21:27 +0200

    Better Client test coverage

--- tests/src/ClientTest.php
+++ tests/src/ClientTest.php
@@ -40,4 +40,154 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
         $select = new Client($guzzle);
     }
+
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Invalid core name
+     */
+    public function testInvalidCore()
+    {
+        $guzzle = m::mock('GuzzleHttp\\Client');
+        $guzzle->shouldReceive('getBaseUrl')
+            ->once()
+            ->andReturn('http://localhost:8983/solr/');
+
+        $client = new Client($guzzle);
+        $client->core([]);
+    }
+
+    public function testCoreStatus()
+    {
+        $guzzle = m::mock('GuzzleHttp\\Client');
+        $response = m::mock('GuzzleHttp\\Message\\Response');
+
+        $guzzle->shouldReceive('getBaseUrl')
+            ->once()
+            ->andReturn('http://localhost:8983/solr/');
+
+        $guzzle->shouldReceive('get')
+            ->with('admin/cores?action=STATUS&wt=json')
+            ->once()
+            ->andReturn($response);
+
+        $response->shouldReceive('json')
+            ->andReturn(123);
+
+        $client = new Client($guzzle);
+        $status = $client->coreStatus();
+
+        $this->assertSame(123, $status);
+    }
+
+    public function testSingleCoreStatus()
+    {
+        $guzzle = m::mock('GuzzleHttp\\Client');
+        $response = m::mock('GuzzleHttp\\Message\\Response');
+
+        $guzzle->shouldReceive('getBaseUrl')
+            ->once()
+            ->andReturn('http://localhost:8983/solr/');
+
+        $guzzle->shouldReceive('get')
+            ->with('admin/cores?action=STATUS&wt=json&name=foo')
+            ->once()
+            ->andReturn($response);
+
+        $response->shouldReceive('json')
+            ->andReturn(123);
+
+        $client = new Client($guzzle);
+        $status = $client->coreStatus('foo');
+
+        $this->assertSame(123, $status);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Invalid core name
+     */
+    public function testCoreStatusInvalidName()
+    {
+        $guzzle = m::mock('GuzzleHttp\\Client');
+        $guzzle->shouldReceive('getBaseUrl')
+            ->once()
+            ->andReturn('http://localhost:8983/solr/');
+
+        $client = new Client($guzzle);
+        $client->coreStatus([]);
+    }
+
+    public function testPing()
+    {
+        $guzzle = m::mock('GuzzleHttp\\Client');
+        $response = m::mock('GuzzleHttp\\Message\\Response');
+
+        $guzzle->shouldReceive('getBaseUrl')
+            ->once()
+            ->andReturn('http://localhost:8983/solr/');
+
+        $guzzle->shouldReceive('get')
+            ->with('admin/ping?wt=json')
+            ->once()
+            ->andReturn($response);
+
+        $expected = 123;
+
+        $response->shouldReceive('json')
+            ->andReturn($expected);
+
+        $client = new Client($guzzle);
+        $actual = $client->ping();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testPingCustomHandler()
+    {
+        $handler = "foo/bar";
+
+        $guzzle = m::mock('GuzzleHttp\\Client');
+        $response = m::mock('GuzzleHttp\\Message\\Response');
+
+        $guzzle->shouldReceive('getBaseUrl')
+            ->once()
+            ->andReturn('http://localhost:8983/solr/');
+
+        $guzzle->shouldReceive('get')
+            ->with("$handler?wt=json")
+            ->once()
+            ->andReturn($response);
+
+        $expected = 123;
+
+        $response->shouldReceive('json')
+            ->andReturn($expected);
+
+        $client = new Client($guzzle);
+        $client->setPingHandler($handler);
+        $actual = $client->ping();
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testGetEmitter()
+    {
+        $guzzle = m::mock('GuzzleHttp\\Client');
+
+        $expected = new \stdClass();
+
+        $guzzle->shouldReceive('getBaseUrl')
+            ->once()
+            ->andReturn('http://localhost:8983/solr/');
+
+        $guzzle->shouldReceive('getEmitter')
+            ->once()
+            ->andReturn($expected);
+
+        $client = new Client($guzzle);
+        $actual = $client->getEmitter();
+
+        $this->assertSame($expected, $actual);
+    }
 }
