87baa1cfe5ce4a9e3277bcddf5c72e246f4cd901
Ivan Habunek <ivan.habunek@gmail.com>
2015-07-09 09:31:38 +0200

    Removed unused use statements

--- src/Console/Commands/StatusCommand.php
+++ src/Console/Commands/StatusCommand.php
@@ -21,9 +21,7 @@ use Opendi\Solr\Client\Console\AbstractCommand;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class StatusCommand extends AbstractCommand
 {
